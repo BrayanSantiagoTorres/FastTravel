217057a4793e7d951922fdde1eff9c23a7bbed99
unknown <iinf18.bsantiagot@itesco.edu.mx>
2022-04-01 13:53:12 -0600

    validate

--- Documents/Aplicaciones Móviles/FastTravel/app/components/Account/RegisterForm.js
+++ Documents/Aplicaciones Móviles/FastTravel/app/components/Account/RegisterForm.js
@@ -1,33 +1,81 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { StyleSheet, View, Text } from 'react-native'
 import { Input, Icon, Button } from 'react-native-elements'
+import { validateEmail } from '../../utils/validation'
 
 export default function RegisterForm(){
+    const [showPassword, setShowPassword] = useState(false)
+    const [showRepeatPassword, setShowRepeatPassword] = useState(false)
+    const [formData, setFormData] = useState(defaultFormValues())
+
+    const onSubmit = () => {
+        if(formData.email.length===0||formData.password.length===0||formData.repeatPassword.length===0){
+            console.log('Debes rellenar los campos vacíos')
+        } else if (!validateEmail(formData.email)){
+            console.log('El correo proporcionado no es correcto')
+        } else if (formData.password !== formData.repeatPassword){
+            console.log('Tus contraseñas deben ser iguales')
+        } else if (formData.password.length < 6){
+            console.log('Tu contraseña debe tener un mínimo de 6 carácteres')
+        } else{
+            console.log('Muy bien')
+        }
+    }
+
+    const onChange = (e, type) => {
+        setFormData({...formData,[type]: e.nativeEvent.text})
+    }
+
     return(
         <View style={styles.formContainer}>
             <Input
                 placeholder='Correo Electronico'
                 containerStyle={styles.inputForm}
+                onChange={(e)=>onChange(e, 'email')}
+                rightIcon={<Icon type= 'material-community' name='email' iconStyle={styles.iconRight}/>}
             />
             <Input
                 placeholder='Contraseña'
                 containerStyle={styles.inputForm}
                 password={true}
-                secureTextEntry={true}
+                secureTextEntry={showPassword ? false : true}
+                onChange={(e)=>onChange(e, 'password')}
+                rightIcon={<Icon 
+                    type= 'material-community' 
+                    name={showPassword ? 'eye-off-outline' : 'eye-outline'} 
+                    iconStyle={styles.iconRight}
+                    onPress={()=> setShowPassword(!showPassword)}
+            />}
             />
             <Input
                 placeholder='Repite la contraseña'
                 containerStyle={styles.inputForm}
                 password={true}
-                secureTextEntry={true}
+                secureTextEntry={showRepeatPassword ? false : true}
+                onChange={(e)=>onChange(e, 'repeatPassword')}
+                rightIcon={<Icon 
+                    type= 'material-community' 
+                    name={showRepeatPassword ? 'eye-off-outline' : 'eye-outline'} 
+                    iconStyle={styles.iconRight}
+                    onPress={()=> setShowRepeatPassword(!showRepeatPassword)}
+            />}
             />
             <Button
                 title='Quiero unirme'
                 containerStyle={styles.btnContainerRegister}
                 buttonStyle={styles.btnRegister}
+                onPress={onSubmit}
             />
         </View>
     )
+}
+
+function defaultFormValues(){
+    return{
+        email: '',
+        password: '',
+        repeatPassword: ''
+    }
 }
 
 const styles = StyleSheet.create({
@@ -45,5 +93,8 @@ const styles = StyleSheet.create({
     btnRegister:{
         backgroundColor:'#0DB8EB',
         borderRadius: 25
+    },
+    iconRight:{
+        color: '#c1c1c1'
     }
 })
