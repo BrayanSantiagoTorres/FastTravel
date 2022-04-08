652b3c10b0764aa83d564e5bd38c6e10294d7f24
unknown <iinf18.bsantiagot@itesco.edu.mx>
2022-04-07 23:20:39 -0500

    cerrar sesion

---
+++ Documents/Aplicaciones Móviles/FastTravel/app/components/Account/InfoUser.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function InfoUser() {
+  return (
+    <View>
+      <Text>HOLASssssss</Text>
+    </View>
+  )
+}
