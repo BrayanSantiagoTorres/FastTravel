be624f636582ebc762771e9113f2418fff7dd697
unknown <iinf18.bsantiagot@itesco.edu.mx>
2022-04-08 02:04:33 -0500

    Foto de perfil y estilos

--- Documents/Aplicaciones Móviles/FastTravel/app/components/Account/InfoUser.js
+++ Documents/Aplicaciones Móviles/FastTravel/app/components/Account/InfoUser.js
@@ -1,10 +1,50 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Avatar } from 'react-native-elements'
 
-export default function InfoUser() {
+export default function InfoUser(props) {
+  const {userInfo} = props
+  const {photoURL, displayName, email} = userInfo
+  console.log(photoURL)
+  console.log(displayName)
+  console.log(email)
   return (
-    <View>
-      <Text>HOLASssssss</Text>
+    <View style={styles.viewUserInfo}>
+      <Avatar
+        title='BRAYAN'
+        rounded
+        size='large'
+        containerStyle={styles.userInfoAvatar}
+        source={
+          photoURL ? {uri:photoURL} : require('../../../assets/img/avatardefFT.jpg')
+      }
+      />
+      <View>
+            <Text style={styles.displayName}>
+                {displayName ? displayName : 'Usuario Invitado'}
+            </Text>
+            <Text>{email ? email : 'Entrada sin autenticación'}</Text>
+        </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  viewUserInfo:{
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexBasis: 'row',
+      backgroundColor: '#f2f2f2',
+      paddingTop: 30,
+      paddingBottom: 30
+    },
+    userInfoAvatar:{
+      marginTop: 20,
+      backgroundColor: '#00a680'
+    },
+    displayName:{
+        fontWeight:'bold',
+        paddingBottom: 5,
+        textAlign: 'center'
+    }
+})
