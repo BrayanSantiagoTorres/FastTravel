00b0a86feee2936a6c07825e10818ffe2c4d1851
Carlos9900 <85185379+Carlos9900@users.noreply.github.com>
2022-06-21 16:05:17 -0500

    colores nuevos

--- app/screens/Main.js
+++ app/screens/Main.js
@@ -12,6 +12,6 @@ export default function Main(){
 const styles = StyleSheet.create({
     viewBody:{
         flex:1,
-        backgroundColor:'#FA8072'
+        backgroundColor:'#00FF7F'
     }
 })
