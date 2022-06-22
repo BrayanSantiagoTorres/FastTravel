d37d9651d3d8edf6879e1834c23598aab3b45971
Carlos9900 <85185379+Carlos9900@users.noreply.github.com>
2022-06-22 01:25:43 -0500

    nuevo diseño

--- app/screens/Main.js
+++ app/screens/Main.js
@@ -1,17 +1,54 @@
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import React, {useState, useEffect} from 'react'
+import { StyleSheet, View, Text, Image } from 'react-native'
+import {Icon} from 'react-native-elements'
+import firebase from 'firebase/app'
 
 export default function Main(){
     return(
         <View style={styles.viewBody}>
-            <Text>Main travels</Text>
-        </View>
+        <Text style={styles.title}>Descubre los mejores detinos para viajar</Text>
+        <Image
+            style={styles.stretch}
+            source={require('../../assets/icon.png')}
+        />
+        <Image
+            style={styles.stretch}
+            source={require('../../assets/img/img333.jpg')}
+        />
+        <Image
+            style={styles.stretch}
+            source={require('../../assets/img/img333.jpg')}
+        />
+    </View>
+    
     )
+    
 }
 
 const styles = StyleSheet.create({
     viewBody:{
         flex:1,
-        backgroundColor:'#F6FFE8'
+        backgroundColor:'#00FF7F'
+    },
+    stretch:{
+        width: '90%',
+        height: 100,
+        resizeMode: 'contain',
+        marginBottom: 110,
+        marginLeft:'250px',
+        borderRadius:'23px'
+    },
+    title:{
+        fontWeight:'bold',
+        fontSize: 30,
+        marginBottom:14,
+        textAlign:'center',
+        backgroundColor: '#778899',
+        fontFamily: 'Helvética'
+    },
+    Image:{
+        width: '250px',
+        borderRadius:'23px'
     }
+   
 })
